0dba514db9a1c97b4d9fcadd23e89a950af83425
Yuki Wicaksono <yukiwicaksono03@gmail.com>
2024-06-20 07:54:04 +0700

    footer

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -212,7 +212,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Ramdani Syaputra 2020</span>
+            <span>Copyright &copy; WIOS 2024</span>
           </div>
         </div>
       </footer>
